5a9d91b9d21bda203193958ab2b6791c758e181c
Luke Lefebure <lefebure@stanford.edu>
2016-02-01 17:44:40 -0800

    added basic functionality

---
+++ R/rNBA.R
@@ -0,0 +1,7 @@
+#' An R package for retrieving stats from the NBA.
+#'
+#' @seealso \code{\link{getPlayerData}}
+#' @docType package
+#' @references All data comes from \url{http://stats.nba.com/}.
+#' @name rNBA
+NULL
