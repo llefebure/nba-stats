92e71e56c4b41da8bdb24fff5d527ee19a51c708
Luke Lefebure <lefebure@stanford.edu>
2016-02-12 12:05:39 -0800

    added endpoint search functionality

--- R/retrieveData.R
+++ R/retrieveData.R
@@ -1,3 +1,5 @@
+# Author: Luke Lefebure
+
 #' Retrieves a data frame containing info for a player.
 #'
 #' @param playerID, ID of player you want to retrieve
@@ -9,16 +11,18 @@ getPlayerData <- function(playerID){
   getGenericData("commonplayerinfo", list(playerID = playerID))
 }
 
-#' Retrieves a generic data source
+#' Retrieve a generic data source
 #'
-#' @param resource, endpoint to retrieve as a character vector
+#' @description Use \link{\code{searchEndpoints}} and \link{\code{getEndpointParams}} to learn
+#' what arguments to pass into this function.
+#' @param endpoint, endpoint to retrieve as a character vector
 #' @param params, list of parameters
 #' @return A data frame or a list of data frames containing the requested data.
 #' @export
 #' @examples
 #' getGenericData("commonallplayers", list(IsOnlyCurrentSeason = 0, LeagueID="00", Season="2015-16"))
-getGenericData <- function(resource, params = list()){
-  url <- buildURL(resource, params)
+getGenericData <- function(endpoint, params = list()){
+  url <- buildURL(endpoint, params)
   data <- jsonlite::fromJSON(url)
   headers <- data$resultSets$headers
   values <- data$resultSets$rowSet
@@ -36,15 +40,47 @@ getGenericData <- function(resource, params = list()){
 
 #' Builds a URL for requesting a resource.
 #'
-#' @param resource, endpoint to retrieve as a character vector
+#' @param endpoint, endpoint to retrieve as a character vector
 #' @param params, list of parameters
 #' @return A full URL for retrieving the specified data.
 #' @examples
 #' buildURL("commonplayerinfo", list(playerID = 201939))
-buildURL <- function(resource, params = list()){
+buildURL <- function(endpoint, params = list()){
   base <- "http://stats.nba.com/stats/"
   param.list <- paste(paste(names(params), unlist(params), sep = "="), collapse = "&")
-  url <- paste0(base, resource)
+  url <- paste0(base, endpoint)
   if (length(params) > 0) url <- paste0(url, "?", param.list)  
   url
+}
+
+#' Search raw endpoints that can be passed to \code{\link{getGenericData}}.
+#'
+#' @description This function lets you search for available endpoints. Use \code{\link{getEndpointParams}}
+#' to get the required parameters for a specified endpoint. Once you have an endpoint and a parameter list,
+#' a call can be made to \code{\link{getGenericData}}.
+#' @param pattern, regex pattern to search for (see \link{regex})
+#' @return A character vector containing all available and matching endpoints.
+#' @export
+#' @examples
+#' searchEndpoints(".")
+#' searchEndpoints("player")
+searchEndpoints <- function(pattern){
+  ind <- grep(pattern, endpoints$Endpoint, ignore.case = TRUE)
+  endpoints$Endpoint[ind]
+}
+
+#' Get raw endpoint parameters to pass to \code{\link{getGenericData}}.
+#'
+#' @description This function returns the expected parameters for a specified endpoint. Use \code{\link{searchEndpoints}}
+#' to search the available endpoints. Once you have an endpoint and a parameter list, a call can be made to 
+#' \code{\link{getGenericData}}.
+#' @param endpoint, a character vector containing an endpoint
+#' @return A character vector describing the required parameters.
+#' @export
+#' @examples
+#' getEndpointParams("commonplayerinfo")
+getEndpointParams <- function(endpoint){
+  if (length(endpoint) != 1) stop("Must pass in only one endpoint")
+  if (!(endpoint %in% endpoints$Endpoint)) stop("Endpoint doesn't exist")
+  endpoints$Parameter[endpoints$Endpoint == endpoint]
 }
