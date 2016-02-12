a9142486dbde77f0528cc0e7b353040a5790117f
Luke Lefebure <lefebure@stanford.edu>
2016-02-11 20:24:04 -0800

    update docs

--- R/rNBA.R
+++ R/rNBA.R
@@ -1,6 +1,6 @@
 #' An R package for retrieving stats from the NBA.
 #'
 #' @docType package
-#' @references All data comes from \url{http://stats.nba.com/}. If you plan to publish anything resulting from the use of this package, the data must be explicity be attributed to the NBA.
+#' @references All data comes from \url{http://stats.nba.com/}. If you plan to publish anything resulting from the use of this package, the data must explicity be attributed to the NBA.
 #' @name rNBA
 NULL
