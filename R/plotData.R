ed9cd310c1c13e5f1a4d40d25e27e0809f7d1778
Luke Lefebure <lefebure@stanford.edu>
2016-02-13 13:04:44 -0800

    added plotData.R

---
+++ R/plotData.R
@@ -0,0 +1,3 @@
+params <- list(SeasonType="Regular+Season", TeamID=0, PlayerID=201939, GameID="", Outcome="", Location="", Month=0, SeasonSegment="", DateFrom="", DateTo="", OpponentTeamID=0, VsConference="", VsDivision="", Position="", RookieYear="", GameSegment="", Period=0, LastNGames=10, ContextMeasure="FGA", Season="2015-16")
+d <- getGenericData("shotchartdetail", params)
+plot(as.numeric(d[[1]]$LOC_X), as.numeric(d[[1]]$LOC_Y))
