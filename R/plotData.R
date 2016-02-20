c2d8efd8cd69b890ce02e6983b1bbb093a6f8d03
Luke Lefebure <lefebure@stanford.edu>
2016-02-19 16:30:30 -0800

    change plotData so that package compiles

--- R/plotData.R
+++ R/plotData.R
@@ -1,3 +1,5 @@
-params <- list(SeasonType="Regular+Season", TeamID=0, PlayerID=201939, GameID="", Outcome="", Location="", Month=0, SeasonSegment="", DateFrom="", DateTo="", OpponentTeamID=0, VsConference="", VsDivision="", Position="", RookieYear="", GameSegment="", Period=0, LastNGames=10, ContextMeasure="FGA", Season="2015-16")
-d <- getGenericData("shotchartdetail", params)
-plot(as.numeric(d[[1]]$LOC_X), as.numeric(d[[1]]$LOC_Y))
+shotChart <- function(){
+  params <- list(SeasonType="Regular+Season", TeamID=0, PlayerID=201939, GameID="", Outcome="", Location="", Month=0, SeasonSegment="", DateFrom="", DateTo="", OpponentTeamID=0, VsConference="", VsDivision="", Position="", RookieYear="", GameSegment="", Period=0, LastNGames=10, ContextMeasure="FGA", Season="2015-16")
+  d <- getGenericData("shotchartdetail", params)
+  plot(as.numeric(d[[1]]$LOC_X), as.numeric(d[[1]]$LOC_Y))  
+}
