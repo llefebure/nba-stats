8874c16ff37e702409a81374e759646ec67325f2
Luke Lefebure <lefebure@stanford.edu>
2016-03-13 16:09:01 -0700

    added player bio data

---
+++ R/playerInfo2016.R
@@ -0,0 +1,12 @@
+# Author: Luke Lefebure
+
+#' Info for active NBA players as of March 2016
+#'
+#' @description A data frame containing info such as country, college, height,
+#' weight, position, etc. for all active NBA players as of March 2016. This
+#' data was retrieved with calls to \code{\link{getGenericData}} using the
+#' "commonplayerinfo" endpoint for every active player whose ID's were found
+#' with \code{\link{getIDMappings}}.
+#' @name playerInfo2016
+#' @docType data
+NULL
