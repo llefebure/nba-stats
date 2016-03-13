f873801138541a9d68c644dca5ff0b27745e5511
Luke Lefebure <lefebure@stanford.edu>
2016-03-13 10:50:26 -0700

    updated data

---
+++ R/draftHistory.R
@@ -0,0 +1,10 @@
+# Author: Luke Lefebure
+
+#' Draft History Data
+#'
+#' @description A data frame with NBA draft picks from 1947 to 2015. This data
+#' was retrieved with a call to \code{\link{getGenericData}} using the "drafthistory"
+#' endpoint.
+#' @name draftHistory
+#' @docType data
+NULL
--- R/stephCurryShots.R
+++ R/stephCurryShots.R
@@ -1,8 +1,11 @@
+# Author: Luke Lefebure
+
 #' Steph Curry Shots Data
 #'
 #' @description A data frame with all of Steph Curry's shots for a 10 game 
 #' period in March 2016. This data is used as a default when building shot
-#' charts with \code{\link{shotChart}}.
+#' charts with \code{\link{shotChart}}. This data was retrieved with a call
+#' to \code{\link{getGenericData}} using the "shotchartdetail" endpoint.
 #' @name stephCurryShots
 #' @docType data
 NULL
