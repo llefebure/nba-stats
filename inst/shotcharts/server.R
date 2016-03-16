5c2bf45082d460377852f3d6e4fc68fa9e5658d3
vinahluw <vinahluw@stanford.edu>
2016-03-15 18:11:53 -0700

    shot chart app complete

--- inst/shotcharts/server.R
+++ inst/shotcharts/server.R
@@ -4,47 +4,37 @@ library(shiny)
 
 `%>%` <- dplyr::`%>%`
 d <- rNBA::stephCurryShots
+# The following gives the halfCoutPoint cooridnates
 halfCourtPoint <- 417.5
+
+# The following outputs per the input chosen
 shinyServer(function(input, output) {
-  
-  d <- reactiveValues(values = stephCurryShots)
-  
-# Create reactive data later    
-#   filtered.f <- reactive({
-#     x <- d %>% dplyr::filter(ORGANIZATION == input$college, 
-#                              SEASON >= as.character(input$dateRange[1]), 
-#                              SEASON <= as.character(input$dateRange[2]))
-#   })
-  
+
+  # The following makes the reactive data  
+  d <- reactiveValues(values = rNBA::stephCurryShots)
+
+  # The following makes the color attribute reactive
+  # color_type <- reactiveValues(values = )
+    
+  # The following makes the plot reactive
   plot <- reactive({
+    # The following decides if the court needs to be full or half
     full <- ifelse(max(as.numeric(d$values$LOC_Y)) > halfCourtPoint, TRUE, FALSE)
-      p <- courtOutlinePlot(full) + ggplot2::geom_point(data = d$values, ggplot2::aes(x = as.numeric(LOC_X), y = as.numeric(LOC_Y), color = EVENT_TYPE))
-      #     p <- ggplot2::ggplot(filtered.f(), ggplot2::aes(x = reorder(TEAM_ABBREVIATION, TEAM_ABBREVIATION, 
-#                                                                 function(x) -length(x)))) + 
-#       ggplot2::geom_bar() + 
-#       ggplot2::labs(x = "Team", y = "Number of Picks", title = input$college)
+    
+    # The following plots the shot chart with the ggplot points
+    p <- rNBA::courtOutlinePlot(full) + ggplot2::geom_point(data = d$values, 
+                                                            ggplot2::aes_string(x = "as.numeric(LOC_X)", 
+                                                                                y = "as.numeric(LOC_Y)", 
+                                                                                color = input$colorattr)) + 
+      ggplot2::theme(legend.title = ggplot2::element_blank())
   })
   
-#   output$playerTable <- renderTable(
-#     dplyr::select(filtered.f(), PLAYER_NAME, SEASON, ROUND_NUMBER, ROUND_PICK, 
-#                   OVERALL_PICK, TEAM_CITY, TEAM_NAME, TEAM_ABBREVIATION))
-  
-#   output$teamKey <- renderTable(
-#     filtered.f() %>% 
-#       dplyr::mutate(TEAM = paste(TEAM_CITY, TEAM_NAME), ABBR = TEAM_ABBREVIATION) %>%
-#       dplyr::distinct(TEAM) %>%
-#       dplyr::select(TEAM, ABBR) %>%
-#       dplyr::arrange(ABBR))
-  
-  
-  
+  # The following takes in the player names
   observeEvent(input$Player, {
-    
     # to get the ID of a selected player name
     selected <- input$Player
     id <- rNBA::searchIDMappings(player = selected)
     # to get data for plotting
-    
     default.params <- list(SeasonType = "Regular+Season", TeamID = 0, PlayerID = id$player$PLAYER_ID, 
                            GameID = "", Outcome = "", Location = "", Month = 0, 
                            SeasonSegment = "", DateFrom = "", DateTo = "", 
@@ -52,15 +42,17 @@ shinyServer(function(input, output) {
                            Position = "", RookieYear = "", GameSegment = "", Period=0, 
                            LastNGames = 30, ContextMeasure = "FGA", Season = "2015-16")
     
-    #shotChartData <- rNBA::getGenericData("shotchartdetail", default.params)
+    # The following uses the memoised version of the getGenericData function
     shotChartData <- rNBA::memGetGenericData("shotchartdetail", default.params) # memoised version
     d$values <- shotChartData[[1]]
   })
   
-    output$plot <- renderPlot({
+  # The following renders the plot of the shot chart
+  output$plot <- renderPlot({
     print(plot())
   })
   
+  # The following creates functionality for downloading the plot
   output$downloadPlot <- downloadHandler(
     filename = "shotchart.png",
     content = function(file) {
--- inst/shotcharts/ui.R
+++ inst/shotcharts/ui.R
@@ -4,46 +4,41 @@ library(shiny)
 
 `%>%` <- dplyr::`%>%`
 
-#d <- rNBA::stephCurryShots
-# d <- rNBA::draftHistory
-# colleges <- d %>% 
-#   dplyr::filter(ORGANIZATION_TYPE == "College/University") %>%
-#   dplyr::select(ORGANIZATION) %>%
-#   dplyr::distinct(ORGANIZATION) %>%
-#   dplyr::arrange(ORGANIZATION)
-
 # get valid names
 m <- rNBA::getIDMappings()
 
 # valid player name options
 player.names <- m$player$PLAYER_NAME[m$player$ROSTERSTATUS == 1]
 
+# valid collor attributes
+color.attr <- c(Quarter = "PERIOD", Area = "SHOT_ZONE_AREA", Outcome = "EVENT_TYPE")
+
+# The following initializes the user interface
 shinyUI(fluidPage(
   
+  # The following makes the title panel
   titlePanel("Player ShotCharts"),
   sidebarLayout(
     sidebarPanel(
+      
+      # The following initializes an input behavior for the player names 
       selectInput("Player",
                   "Player Name:",
                   choices = player.names,
                   selected = "Stephen Curry"),
       
-#       dateRangeInput("dateRange",
-#                      label = "Date Range",
-#                      start = as.Date("1947", format = "%Y"),
-#                      end = as.Date("2015", format = "%Y"),
-#                      min = as.Date(min(d$SEASON), format = "%Y"), 
-#                      max = as.Date(max(d$SEASON), format = "%Y"),
-#                      separator = " - ", format = "yyyy",
-#                      startview = "year"),
-       
-      downloadButton("downloadPlot", label = "Download Plot"),
-      tableOutput("teamKey")
+      # The following initializes an input behavior for the type of chart 
+      selectInput("colorattr",
+                  "Color Attribute:",
+                  choices = color.attr,
+                  selected = "EVENT_TYPE"),
+      
+      # The following  creates a download button
+      downloadButton("downloadPlot", label = "Download Plot")
     ),
     
     mainPanel(
-      plotOutput("plot"),
-      tableOutput("playerTable")
+      plotOutput("plot")
     )
   )
 ))
